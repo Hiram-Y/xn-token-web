be5ab9f0593b89c58234c1f8322e6be7c414d98e
hss1994 <1070798770@qq.com>
2018-03-14 19:55:43 +0800

    110版本新加web端

---
+++ static/js/app/config.js
@@ -0,0 +1,13 @@
+var SYSTEM_CODE = "CD-TOKEN00018";
+var COMPANY_CODE = "CD-TOKEN00018";
+var PIC_PREFIX = 'http://p5k7690z2.bkt.clouddn.com/';
+var PHOTO_SUFFIX = '?imageMogr2/auto-orient/thumbnail/!150x150r';
+var THUMBNAIL_SUFFIX ="";
+// web 域名 测试
+var DOMAIN_NAME = 'http://www.bcointest.hichengdai.com';
+// h5 域名 测试
+var INVITATION_HREF = 'http://mtest.bcoin.hichengdai.com';
+var ZENDESK_LABEL = 'search';
+
+var FOOT_TETUI= 'Bcoin Exchange';
+var FOOT_EMAIL = 'contact@bcoin.im';
---
+++ static/js/app/interface/BaseCtr.js
@@ -0,0 +1,14 @@
+define([
+    'app/controller/base',
+    'app/util/ajax'
+], function(base, Ajax) {
+    return {
+        // 获取币种列表
+        getCoinList() {
+        	return ;
+//          return Ajax.get("802503", {
+//              userId: base.getUserId()
+//          }, true);
+        },
+    };
+})
